ed3cc9f400253d7cf17a845da980ea2e863c987c
elfadili-ae <elfadili.ae@gmail.com>
2023-07-22 16:24:08 +0100

    Fix: counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -17,18 +17,18 @@ void counting_sort(int *array, size_t size)
 		if (largest < array[i])
 			largest = array[i];
 
-	countArr = (int *)malloc(sizeof(int) * largest);
-	for (i = 0; i <= (size_t)largest; i++)
+	countArr = malloc(sizeof(int) * largest);
+	for (i = 0; i < (size_t)largest; i++)
 		countArr[i] = 0;
 
 	for (i = 0; i < size; i++)
 		countArr[array[i]] += 1;
 
-	for (i = 1; i <= (size_t)largest; i++)
-		countArr[i] += countArr[i - 1];
+	for (i = 0; i < (size_t)largest; i++)
+		countArr[i + 1] += countArr[i];
 
 	print_array(countArr, largest + 1);
-	newArr = (int *)malloc(sizeof(int) * size);
+	newArr = malloc(sizeof(int) * size);
 	for (i = 0; i <= size; i++)
 		newArr[i] = 0;
 
@@ -38,6 +38,8 @@ void counting_sort(int *array, size_t size)
 		countArr[array[i]] -= 1;
 	}
 	for (i = 0; i < size; i++)
-	array[i] = newArr[i];
+		array[i] = newArr[i];
 
+	free(countArr);
+	free(newArr);
 }
